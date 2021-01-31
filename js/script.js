ba0d95d222459918b4296fd13179048acb3ab4a9
kevinralph <kevin.iglesias@r4pid.com>
2021-01-31 14:20:55 +0800

    apply slider insight

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-
+    $('.slider-insights').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: false,
+        prevArrow: false,
+        nextArrow: false,
+        responsive: [
+            {
+              breakpoint: 640,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+        ],
+    });
 });
